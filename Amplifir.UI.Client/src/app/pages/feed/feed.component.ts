ad3ac7240f2c583e19c4be64e13a2225d3c3548a
João Neves <joao95neves@gmail.com>
2019-11-27 22:12:28 +0000

    [ UI.Client ]: Set up the Feed page

---
+++ Amplifir.UI.Client/src/app/pages/feed/feed.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './feed.component.html',
+  styleUrls: ['./feed.component.scss']
+})
+export class FeedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
