d55ca1dd69766a7d027fe464b1463f97627a5b6b
João Neves <joao95neves@gmail.com>
2019-11-27 22:15:28 +0000

    [ UI.Client ]: Set up basic app structure (navbar + pages)

--- Amplifir.UI.Client/src/app/app.module.ts
+++ Amplifir.UI.Client/src/app/app.module.ts
@@ -3,20 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './common/navbar/navbar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { RouterLinkComponent } from './common/router-link/router-link.component';
+import { FeedComponent } from './pages/feed/feed.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ProfileComponent
+    ProfileComponent,
+    RouterLinkComponent,
+    FeedComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [RouterLinkComponent]
 })
 export class AppModule { }
