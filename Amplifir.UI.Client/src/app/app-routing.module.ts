19c93e32538cb06ab16866d70741c2065b66b99c
João Neves <joao95neves@gmail.com>
2019-11-27 22:15:50 +0000

    [ UI.Client ]: Set up router

--- Amplifir.UI.Client/src/app/app-routing.module.ts
+++ Amplifir.UI.Client/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ProfileComponent } from './pages/profile/profile.component';
+import { FeedComponent } from './pages/feed/feed.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'feed', pathMatch: 'full' },
+  { path: 'feed', component: FeedComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'settings', component: SettingsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
