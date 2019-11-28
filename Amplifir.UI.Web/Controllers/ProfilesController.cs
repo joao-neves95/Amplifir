21e2250362e7dd168eecd601954f6cea4a2b93b4
João Neves <joao95neves@gmail.com>
2019-11-28 00:09:36 +0000

    [ UI.Web ]: Set up the profiles controller

---
+++ Amplifir.UI.Web/Controllers/ProfilesController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Amplifir.UI.Web.Controllers
+{
+    [Route("api/profiles")]
+    [ApiController]
+    public class ProfilesController : ControllerBase
+    {
+        /// <summary>
+        /// 
+        /// <para>
+        ///   Gets the requested user profile.
+        ///   Sending -1 as the id, gets the current authenticated user.
+        /// </para>
+        /// 
+        /// <code>
+        ///   GET: [controller-route]/69
+        /// </code>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}", Name = "Get")]
+        public string Get([FromRoute]int id)
+        {
+            // Get the claim id from the JWT auth token, if the id == -1.
+            return "value";
+        }
+
+        /// <summary>
+        /// 
+        /// <para>
+        ///   Updates the complete profile of the current authenticated user.
+        /// </para>
+        /// 
+        /// <code>
+        ///   PUT: [controller-route]
+        /// </code>
+        /// 
+        /// </summary>
+        [HttpPut]
+        public void Put()
+        {
+            // Get the claim id from the JWT auth token.
+        }
+
+        /// <summary>
+        /// 
+        /// <para>
+        ///   Deletes the profile of the current authenticated user.
+        /// </para>
+        /// 
+        /// <code>
+        ///   DELETE: [controller-route]
+        /// </code>
+        /// 
+        /// </summary>
+        [HttpDelete]
+        public void Delete()
+        {
+            // Get the claim id from the JWT auth token.
+        }
+    }
+}
