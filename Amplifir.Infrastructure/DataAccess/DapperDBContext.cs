0a32fc765798745ec81f4a4ff3028a8644d84586
João Neves <joao95neves@gmail.com>
2019-12-01 19:22:10 +0000

    [ Infrastructure ]: Creating the DBContext base

---
+++ Amplifir.Infrastructure/DataAccess/DapperDBContext.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using System;
+using System.Data.Common;
+using Npgsql;
+using Amplifir.Infrastructure.DataAccess.Interfaces;
+
+namespace Amplifir.Infrastructure.DataAccess
+{
+    public class DapperDBContext : IDBContext
+    {
+        #region IDisposable Support
+
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                this._dbConnection.Dispose();
+                disposedValue = true;
+            }
+        }
+
+        ~DapperDBContext()
+        {
+            Dispose( false );
+        }
+
+        public void Dispose()
+        {
+            Dispose( true );
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion IDisposable Support
+
+        protected DbConnection _dbConnection;
+
+        public async Task<DbConnection> OpenDBConnectionAsync(string connectionString)
+        {
+            this._dbConnection = new NpgsqlConnection( connectionString );
+            await this._dbConnection.OpenAsync();
+            return this._dbConnection;
+        }
+    }
+}
---
+++ Amplifir.Infrastructure/DataAccess/Interfaces/IDBContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Data.Common;
+
+namespace Amplifir.Infrastructure.DataAccess.Interfaces
+{
+    interface IDBContext : IDisposable
+    {
+        Task<DbConnection> OpenDBConnectionAsync(string connectionString);
+    }
+}
