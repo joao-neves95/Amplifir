893ac2525605925c7bbd7392f9b3bda9e6767bb8
João Neves <joao95neves@gmail.com>
2019-12-02 01:03:59 +0000

    [ Infrastructure/DataAccess ]: Added the ShoutStore

---
+++ Amplifir.Infrastructure/DataAccess/Stores/ShoutStore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Amplifir.Core.Interfaces;
+
+namespace Amplifir.Infrastructure.DataAccess.Stores
+{
+    public class ShoutStore : DBStoreBase
+    {
+        public ShoutStore(IDBContext dBContext) : base( dBContext )
+        {
+        }
+    }
+}
