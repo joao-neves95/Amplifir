46c50950fd00d0f98e4b3131e245cc788fbb0f26
João Neves <joao95neves@gmail.com>
2019-12-03 15:10:52 +0000

    [ Core/Entities ]: Update AppUser

--- Amplifir.Core/Entities/AppUser.cs
+++ Amplifir.Core/Entities/AppUser.cs
@@ -10,5 +10,11 @@ namespace Amplifir.Infrastructure.Entities
         public int Id { get; set; }
 
         public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string PhoneNumber { get; set; }
     }
 }
