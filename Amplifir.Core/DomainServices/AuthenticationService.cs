eaf354d7d3901cbbfa182b3ea04d0fd3fc73cf74
João Neves <joao95neves@gmail.com>
2019-12-16 20:27:03 +0000

    [ Core/AuthenticationService ]: Prepared the basic methods.

--- Amplifir.Core/DomainServices/AuthenticationService.cs
+++ Amplifir.Core/DomainServices/AuthenticationService.cs
@@ -1,11 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Amplifir.Core.Interfaces;
+using Amplifir.Core.Entities;
 
 namespace Amplifir.Core.DomainServices
 {
     public class AuthenticationService : IAuthenticationService
     {
+        private readonly IAppUserStore<AppUser, int> _appUserStore;
+
+        public AuthenticationService(IAppUserStore<AppUser, int> appUserStore)
+        {
+            this._appUserStore = appUserStore;
+        }
+
+        public Task LoginUser(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RegisterUser(string email, string password)
+        {
+            // 1 - Check if the email already exists.
+            // 2 - Check the password's minimun length.
+            // 3 - Create user.
+            throw new NotImplementedException();
+        }
     }
 }
--- Amplifir.Core/Interfaces/IAuthenticationService.cs
+++ Amplifir.Core/Interfaces/IAuthenticationService.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Amplifir.Core.Interfaces
 {
+    /// <summary>
+    /// 
+    /// Service for authentication and authorization.
+    /// 
+    /// </summary>
     public interface IAuthenticationService
     {
+        Task RegisterUser( string email, string password );
+
+        Task LoginUser( string email, string password );
     }
 }
