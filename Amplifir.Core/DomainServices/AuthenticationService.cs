e8ab320de3de0d7e4ebd0b02bd81fd64d2276a7f
João Neves <joao95neves@gmail.com>
2019-12-16 19:58:50 +0000

    [ Core/DomainServices ]: Prepared the authentication service classes.

---
+++ Amplifir.Core/DomainServices/AuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Amplifir.Core.Interfaces;
+
+namespace Amplifir.Core.DomainServices
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+    }
+}
---
+++ Amplifir.Core/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Amplifir.Core.Interfaces
+{
+    public interface IAuthenticationService
+    {
+    }
+}
