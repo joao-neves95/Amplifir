ff53cb8047b42749b7fec9c3e3e1e2b03c3e5f21
João Neves <joao95neves@gmail.com>
2019-12-19 23:17:57 +0000

    [ Infrastructure/DataAccess ]: Started working on the Shout Store.

--- Amplifir.Core/Interfaces/IShoutStore.cs
+++ Amplifir.Core/Interfaces/IShoutStore.cs
@@ -1,11 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Amplifir.Core.Entities;
 
 namespace Amplifir.Core.Interfaces
 {
     public interface IShoutStore
     {
-        void GetById( int shoutId );
+        /// <summary>
+        /// 
+        /// Insert a new Shout.
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        Task<bool> CreateAsync( Shout newShout );
+
+        /// <summary>
+        /// 
+        /// Get Shout by ID.
+        /// 
+        /// </summary>
+        /// <param name="shoutId"></param>
+        /// <returns></returns>
+        Task<Shout> GetByIdAsync( int shoutId );
+
+        /// <summary>
+        /// 
+        /// Get the last paginated Shouts of a user by ID.
+        /// 
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="lastId"></param>
+        /// <param name="limit"></param>
+        /// <returns></returns>
+        Task<Shout> GetByUserIdAsync( int userId, int lastId = 0, short limit = 10 );
+
+        /// <summary>
+        /// 
+        /// Get the last paginated Shouts from all the users a user follows.
+        /// 
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="lastId"></param>
+        /// <param name="limit"></param>
+        /// <returns></returns>
+        Task<Shout> GetFollowingShoutsByUserIdAsync( int userId, int lastId = 0, short limit = 10 );
     }
 }
--- Amplifir.Infrastructure/DataAccess/Stores/ShoutDapperStore.cs
+++ Amplifir.Infrastructure/DataAccess/Stores/ShoutDapperStore.cs
@@ -1,14 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Amplifir.Core.Entities;
 using Amplifir.Core.Interfaces;
 
 namespace Amplifir.Infrastructure.DataAccess.Stores
 {
-    public class ShoutDapperStore : DBStoreBase
+    public class ShoutDapperStore : DBStoreBase, IShoutStore
     {
         public ShoutDapperStore(IDBContext dBContext) : base( dBContext )
         {
+        }
+
+        public async Task<bool> CreateAsync(Shout newShout)
+        {
+            await base._dBContext.DbConnection.OpenAsync();
+
+            using (base._dBContext.DbConnection)
+            {
+                // TODO: Finish .CreateAsync()
+                await base._dBContext.ExecuteTransactionAsync( new Dictionary<string, object>()
+                {
+                    {
+                        @"INSERT INTO Shout (UserId, Content)
+                          VALUES (@UserId, @Content)
+                        ",
+                        new { @UserId = newShout.UserId, @Content = newShout.Content }
+                    },
+                    {
+                        @"INSERT INTO ShoutAsset ()
+                          VALUES (@)
+                        ",
+                        null
+                    },
+                } );
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public async Task<Shout> GetByIdAsync(int shoutId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Shout> GetByUserIdAsync(int userId, int lastId = 0, short limit = 10)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Shout> GetFollowingShoutsByUserIdAsync(int userId, int lastId = 0, short limit = 10)
+        {
+            throw new NotImplementedException();
         }
     }
 }
