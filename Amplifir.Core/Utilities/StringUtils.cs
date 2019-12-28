2ba478b3aa664889a8f655cf0722b99f4b741dfd
João Neves <joao95neves@gmail.com>
2019-12-28 19:15:21 +0000

    [ Core ]: Added the database connection builder to the StringUtils

---
+++ Amplifir.Core/Utilities/StringUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Amplifir.Core.Utilities
+{
+    public static class StringUtils
+    {
+        // https://www.connectionstrings.com/
+        public static string BuildConnectionString(string server, string port, string database, string userName, string password)
+        {
+            return $"Server={server}; Port={port}; Database={database}; User Id={userName}; Password={password};";
+        }
+
+        public static string BuildConnectionStringWithSSL(string server, string port, string database, string userName, string password)
+        {
+            return StringUtils.BuildConnectionString( server, port, database, userName, password ) + " SslMode=Require;";
+        }
+    }
+}
