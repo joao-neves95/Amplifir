e2c032c51bff8166b922c7801303150961d27f9d
João Neves <joao95neves@gmail.com>
2019-12-28 19:04:40 +0000

    [Core]: Added ApplicationTypeFactory to achieve complete loose-coupling.

---
+++ Amplifir.ApplicationTypeFactory/ApplicationTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Amplifir.ApplicationTypeFactory
+{
+    public enum ApplicationTypes
+    {
+        DapperDBContext,
+        AuthenticationService,
+        AppUserDapperStore
+    }
+}
---
+++ Amplifir.ApplicationTypeFactory/TypeFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Amplifir.Core.Interfaces;
+using Amplifir.Core.Models;
+using Amplifir.Core.DomainServices;
+using Amplifir.Infrastructure.DataAccess;
+
+namespace Amplifir.ApplicationTypeFactory
+{
+    /// <summary>
+    /// 
+    /// The application's Type factory.
+    /// 
+    /// </summary>
+    public static class TypeFactory
+    {
+        public static Type Get( ApplicationTypes applicationType )
+
+        {
+            switch (applicationType)
+            {
+                case ApplicationTypes.DapperDBContext:
+                    return typeof( DapperDBContext );
+
+                case ApplicationTypes.AuthenticationService:
+                    return typeof( AuthenticationService );
+
+                case ApplicationTypes.AppUserDapperStore:
+                    return typeof( AppUserDapperStore );
+
+                default:
+                    throw new TypeAccessException( "Unknown type: " + Enum.GetName( typeof( ApplicationTypes ), applicationType ) );
+            }
+        }
+    }
+}
