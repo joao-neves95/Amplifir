18ee42a9c0530c2afe973ccd397572faf27812f0
João Neves <joao95neves@gmail.com>
2019-12-29 15:07:14 +0000

    [ Web/ ]: Added the AuthController.

---
+++ Amplifir.UI.Web/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amplifir.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Amplifir.UI.Web.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public AuthController(IAuthenticationService authenticationService)
+        {
+            this._authenticationService = authenticationService;
+        }
+
+        private readonly IAuthenticationService _authenticationService;
+
+        public void Register() { }
+
+        public void Login() { }
+
+        public void Logout() { }
+    }
+}
