cfd2e2ded482728a7d83a60594f6cc53e3858a76
João Neves <joao95neves@gmail.com>
2019-12-29 15:09:05 +0000

    [ Core/DomainServices/ ]: Added the ProfileService.

---
+++ Amplifir.Core/DomainServices/ProfileService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Amplifir.Core.Interfaces;
+using Amplifir.Core.Models;
+
+namespace Amplifir.Core.DomainServices
+{
+    public class ProfileService
+    {
+        public ProfileService(IAppUserStore<AppUser, int> appUserStore )
+        {
+            this._appUserStore = appUserStore;
+        }
+
+        private readonly IAppUserStore<AppUser, int> _appUserStore;
+    }
+}
