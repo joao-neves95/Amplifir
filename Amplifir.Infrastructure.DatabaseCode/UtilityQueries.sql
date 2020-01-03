53817ca317b2da7a854a745f8407defeb413c2c9
João Neves <joao95neves@gmail.com>
2020-01-03 23:18:10 +0000

    [ Infrastructure/DatabaseCode ]: Added utility queries.

---
+++ Amplifir.Infrastructure.DatabaseCode/UtilityQueries.sql
@@ -0,0 +1,9 @@
+﻿--select * from appuser;
+--select * from appuserprofile;
+--select * from AuditLog;
+
+-- REGISTER USER.
+--delete from appuserprofile where userid = 14;
+--delete from auditlog where userid = 14;
+--delete from appuser where id = 14;
+-- End of REGISTER USER.
