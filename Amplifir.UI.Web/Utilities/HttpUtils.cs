9a704616ef4b0d96a1216e112cbe1c523487df51
João Neves <joao95neves@gmail.com>
2020-01-03 17:32:34 +0000

    [ Web/Utilities/HttpUtils ]: Added the .GetUserIp() method.

---
+++ Amplifir.UI.Web/Utilities/HttpUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Amplifir.UI.Web.Utilities
+{
+    public class HttpUtils
+    {
+        public static string GetUserIp( HttpContext httpContext )
+        {
+            if (!String.IsNullOrEmpty( httpContext.Request.Headers["X-Forwarded-For"] ) )
+            {
+                // It's possible that it comes multiple addresses.
+                return httpContext.Request.Headers["X-Forwarded-For"].ToString().Split( ',' )[0];
+            }
+            else if (!String.IsNullOrEmpty( httpContext.Request.Headers["CF-Connecting-IP"] ))
+            {
+                return httpContext.Request.Headers["CF-Connecting-IP"].ToString().Split( ',' )[0];
+            }
+            else
+            {
+                return httpContext.Connection.RemoteIpAddress.ToString();
+            }
+        }
+    }
+}
