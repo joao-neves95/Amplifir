e67479f94ba966e55736dd6a9229c602f9029b52
João Neves <joao95neves@gmail.com>
2020-01-04 22:45:52 +0000

    [ Core/Utilities/CSharpExtension ]: Added the Switch extension.

---
+++ Amplifir.Core/Utilities/CSharpExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Amplifir.Core.Utilities
+{
+    public static class CSharpExtensions
+    {
+        public static TReturn Switch<TCase, TReturn>(this TCase @case, Dictionary<TCase, Func<TReturn>> actions, Func<TReturn> defaultFunc)
+        {
+            if (actions.TryGetValue( @case, out Func<TReturn> action ))
+            {
+                return action();
+            }
+            else
+            {
+                return defaultFunc();
+            }
+        }
+    }
+}
