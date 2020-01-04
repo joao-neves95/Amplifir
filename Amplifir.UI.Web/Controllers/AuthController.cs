30c7ce56cae2e490967bebba353ac47f723b955e
João Neves <joao95neves@gmail.com>
2020-01-04 22:47:51 +0000

    [ Web/Controllers/Auth ]: Return the global ApiResponse DTO model.

--- Amplifir.UI.Web/Controllers/AuthController.cs
+++ Amplifir.UI.Web/Controllers/AuthController.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using System.Data.Common;
+using Microsoft.AspNetCore.Mvc;
 using Amplifir.Core.Interfaces;
 using Amplifir.Core.DTOs;
 using Amplifir.Core.Enums;
 using Amplifir.Core.Models;
 using Amplifir.Core.Entities;
+using Amplifir.Core.Utilities;
 using Amplifir.UI.Web.Utilities;
+using Amplifir.UI.Web.Resources;
+
+// TODO: Before production, remove every API exception returns.
 
 namespace Amplifir.UI.Web.Controllers
 {
@@ -15,10 +20,11 @@ namespace Amplifir.UI.Web.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        public AuthController(IAuthenticationService authenticationService, IJWTService jWTService)
+        public AuthController(IAuthenticationService authenticationService, IJWTService jWTService, IAuditLogStore auditLogStore)
         {
             this._authenticationService = authenticationService;
             this._jWTService = jWTService;
+            this._auditLogStore = auditLogStore;
         }
 
         #region PROPERTIES
@@ -27,30 +33,50 @@ namespace Amplifir.UI.Web.Controllers
 
         private readonly IJWTService _jWTService;
 
+        private readonly IAuditLogStore _auditLogStore;
+
         #endregion PROPERTIES
 
         #region METHODS
 
         [HttpPost( "register" )]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage( "Reliability", "CA2007:Consider calling ConfigureAwait on the awaited task", Justification = "<Pending>" )]
         public async Task<IActionResult> Register([FromBody]UserCredentialsDTO userCredentialsDTO)
         {
             try
             {
-                RegisterUserResult registerUserResult = await this._authenticationService.RegisterUserAsync( new AppUser()
+                IAppUser newAppUser = new AppUser()
                 {
                     Email = userCredentialsDTO.Email,
                     Password = userCredentialsDTO.Password,
                     Ipv4 = HttpUtils.GetUserIp( HttpContext )
-                } );
+                };
+
+                RegisterUserResult registerUserResult = await this._authenticationService.RegisterUserAsync( newAppUser );
 
                 if (registerUserResult.State != RegisterUserState.Success)
                 {
                     // TODO: Error handling.
-                    return BadRequest();
+                    return BadRequest( new ApiResponse<bool>()
+                    {
+                        Error = true,
+                        Message = registerUserResult.State.Switch( new Dictionary<RegisterUserState, Func<string>>()
+                        {
+                            { RegisterUserState.EmailExists, () => Resource_ResponseMessages_en.EmailExists },
+                            { RegisterUserState.PasswordTooSmall, () => Resource_ResponseMessages_en.PasswordTooSmall },
+                            { RegisterUserState.InvalidEmail, () => Resource_ResponseMessages_en.InvalidEmail }
+                        },
+                            () => Resource_ResponseMessages_en.Unknown
+                        )
+                    } );
                 }
                 else
                 {
-                    return RedirectToActionPermanent( "login", new UserCredentialsDTO( registerUserResult.User.Email, registerUserResult.User.Password ) );
+                    return Ok( new ApiResponse<LoginResponse>()
+                    {
+                        Message = Resource_ResponseMessages_en.RegisterSuccess,
+                        EndpointResult = new LoginResponse( this._jWTService.Generate( newAppUser ) )
+                    } );
                 }
             }
             catch (DbException e)
@@ -75,12 +101,36 @@ namespace Amplifir.UI.Web.Controllers
                 if (validateSignInResult.State != ValidateSignInState.Success)
                 {
                     // TODO: Error handling.
-                    return BadRequest();
+                    return BadRequest( new ApiResponse<bool>()
+                    {
+                        Error = true,
+                        Message = validateSignInResult.State.Switch( new Dictionary<ValidateSignInState, Func<string>>()
+                        {
+                            { ValidateSignInState.NotFound, () => Resource_ResponseMessages_en.UserNotFound  },
+                            { ValidateSignInState.WrongPassword, () => Resource_ResponseMessages_en.WrongPassword  }
+                        },
+                            () => Resource_ResponseMessages_en.Unknown
+                        )
+                    } );
                 }
                 else
                 {
                     validateSignInResult.User.Ipv4 = HttpUtils.GetUserIp( HttpContext );
-                    return Ok( new LoginResponse( this._jWTService.Generate( validateSignInResult.User ) ) );
+
+                    // Do not await in order to respond as fast as possible,
+                    // the task completion is not necessary for the response creation.
+                    _ = _auditLogStore.CreateLog( new AuditLog()
+                    {
+                        UserId = validateSignInResult.User.Id,
+                        EventTypeId = EventTypeId.Login,
+                        IPv4 = validateSignInResult.User.Ipv4
+                    } );
+
+                    return Ok( new ApiResponse<LoginResponse>()
+                    {
+                        Message = Resource_ResponseMessages_en.LoginSuccess,
+                        EndpointResult = new LoginResponse( this._jWTService.Generate( validateSignInResult.User ) )
+                    } );
                 }
             }
             catch (DbException e)
