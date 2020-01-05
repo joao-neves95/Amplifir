28422ca32e38684f51ad8d3877801ab4871acba7
João Neves <joao95neves@gmail.com>
2020-01-05 20:12:30 +0000

    [ UI/Client ]: Added typescript constants.

---
+++ Amplifir.UI.Client/src/app/shared/constants.ts
@@ -0,0 +1,13 @@
+
+export const Constants = Object.freeze({
+  ids: {
+    modalsComponent: 'modals'
+  },
+  defaultLabels: {
+    authModalTitle: 'Login',
+    shoutModalTitle: 'Shout!'
+  },
+  timings: {
+    authModalInitTime: 3000
+  }
+});
