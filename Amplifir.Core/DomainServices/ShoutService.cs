2c383835adb5ffad9d5c6f3f833185d64d4c46a7
João Neves <joao95neves@gmail.com>
2020-01-06 18:18:41 +0000

    [ Core/ ]: Prepared the Shout service.

---
+++ Amplifir.Core/DomainServices/ShoutService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Amplifir.Core.Entities;
+using Amplifir.Core.Interfaces;
+
+namespace Amplifir.Core.DomainServices
+{
+    public class ShoutService : IShoutService
+    {
+        public Task<bool> Create(Shout newShout)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
