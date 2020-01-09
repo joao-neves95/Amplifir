c43934d269874045032eff679fdde393499f2979
João Neves <joao95neves@gmail.com>
2020-01-09 13:12:25 +0000

    [ (global) ]: (VS stuff)

--- Amplifir.UI.Web.AppSettings/AppSettings.cs
+++ Amplifir.UI.Web.AppSettings/AppSettings.cs
@@ -1,8 +1,5 @@
-﻿using Amplifir.Core.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System;
+using Amplifir.Core.Interfaces;
 
 namespace Amplifir.UI.Web.AppSettings
 {
