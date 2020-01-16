2a55fbe7f49935cd9331ebfb975f9cd38cecc306
João Neves <joao95neves@gmail.com>
2020-01-16 19:43:19 +0000

    [ UI/Presentation/Profile ]: Added the profile header and empty shouts.

--- Amplifir.UI.Presentation/src/app/components/pages/profile/profile.component.ts
+++ Amplifir.UI.Presentation/src/app/components/pages/profile/profile.component.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 - 2020 João Pedro Martins Neves (SHIVAYL) - All Rights Reserved.
+ * Copyright (c) 2019 - 2020 Joï¿½o Pedro Martins Neves (SHIVAYL) - All Rights Reserved.
  *
  * Amplifir and all its content is licensed under the GNU Lesser General Public License (LGPL),
  * version 3, located in the root of this project, under the name "LICENSE.md".
@@ -8,6 +8,8 @@
 
 import { Component, OnInit } from '@angular/core';
 
+import { Constants } from '../../../shared/constants';
+
 @Component({
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
@@ -15,6 +17,18 @@ import { Component, OnInit } from '@angular/core';
 export class ProfileComponent implements OnInit {
 
   constructor() { }
+
+  readonly CONSTANTS = Constants;
+
+  userName: string = Constants.defaultLabels.userName;
+  bio: string = '';
+  location: string = '';
+  birthDate: string = '';
+  website: string = '';
+  followingCount: number = 0;
+  followersCount: number = 0;
+
+  shouts: string[] = [];
 
   ngOnInit() {
   }
