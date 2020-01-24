b4ad7f7b07945329f0fa0d080d9693e4eff11d52
João Neves <joao95neves@gmail.com>
2020-01-24 22:19:16 +0000

    [ Presentation/ ]: Added the shout card.

--- Amplifir.UI.Presentation/src/app/components/shared/shout-card/shout-card.component.ts
+++ Amplifir.UI.Presentation/src/app/components/shared/shout-card/shout-card.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Shout } from '../../../services/apiClient.service';
 
 @Component({
   selector: 'app-shout-card',
@@ -9,7 +11,11 @@ export class ShoutCardComponent implements OnInit {
 
   constructor() { }
 
+  @Input() shout: Shout = new Shout();
+
   ngOnInit() {
+    // Ignore error.
+    this.shout.createDate = new Date( this.shout.createDate ).toLocaleString();
   }
 
 }
